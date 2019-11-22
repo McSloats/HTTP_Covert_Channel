f307e5f837f52055a46a0933a47b1fb6ed6dfca4
McSloats <47727675+McSloats@users.noreply.github.com>
2019-11-22 09:17:46 -0800

    Working Prototype
    Adjusted mouse control for 4x4 grid. Fixed receiver.

--- Project1-Demo/getReader.py
+++ Project1-Demo/getReader.py
@@ -1,30 +1,34 @@
 import pyshark 
-capture = pyshark.LiveCapture(interface='lo0', display_filter='http', only_summaries=True)
+import binascii
+capture = pyshark.LiveCapture(interface='lo', display_filter='http', only_summaries=True)
 message=[]
 check=[]
 start = False
+end = '000000'
 for packet in capture.sniff_continuously():
+    temp = packet.info.split()
     if(not start):
-        temp = packet.info.split()
         if(temp[0]=="GET"):
             check.append(temp[1][2:3])
-        if(len(check)==6): 
+        if(len(check)==5): 
             end = ''.join(check)
-            if(end=='2D2E2D'): 
-                start=True 
+            if(end=='D2D2D'): 
+                start=True
+                check=[]
             else: 
                 check=[]
-    else: 
+    else:
         if(temp[0]=="GET" and temp[1][1:2]=='c' and temp[1][3:4]=='?'): 
             if(len(check)==6): 
-                end = ''.join(check)
-                #print(end)
-                if(end=='2D2E2D'): 
-                    mess=''.join(message)
-                    print(mess.decode("hex"))
-                    break 
-                else: 
-                    check=[]
+                 end = ''.join(check)
+                 if(end=='2D2E2D'):
+                     mess=''.join(message)
+                     print(binascii.unhexlify(mess).decode().rstrip("\n\-\.\-"))
+                     break
+                 else: 
+                     check=check[1:]
+            #else:
             check.append(temp[1][2:3])
             message.append(temp[1][2:3])
-    
+capture.close()
+exit()
