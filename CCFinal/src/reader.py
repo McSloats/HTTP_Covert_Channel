cc5bcab6cbbefead8709027f805ef204f8c786a0
McSloats <47727675+McSloats@users.noreply.github.com>
2020-02-27 15:58:15 -0500

    Presentation Ready
    This is the final version used for the presentation demo. Missing the graphical webpage without grid.

---
+++ CCFinal/src/reader.py
@@ -0,0 +1,22 @@
+import pyshark 
+import os
+import binascii
+capture = pyshark.LiveCapture(interface='lo', display_filter='http', only_summaries=False)
+message=[]
+count= 0
+for packet in capture.sniff_continuously():
+    #GET /c2?4157 HTTP/1.1\r\n
+    packet=str(packet)
+    try:
+        data=packet.split("GET ")[1].split(" ")[0].split("/c")[1].split("?")[0]
+        count += 1 
+        message.append(data)
+        mess=''.join(message)
+        if(count==2):
+            os.system("clear")
+            print(binascii.unhexlify(mess).decode()+" ", end = '')
+            count = 0
+    except:
+        continue
+capture.close()
+exit()
