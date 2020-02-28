3436077cc0a7cda99d5de2b99b63d1a101a85918
MasterYork42 <wdy6430@rit.edu>
2020-02-28 07:42:08 -0500

    Add files via upload
    Code will now wait for a starting sequence before it tries reading the message, once an ending sequence is reach the code will terminate.

--- CCFinal/src/reader.py
+++ CCFinal/src/reader.py
@@ -5,19 +5,28 @@ import sys
 capture = pyshark.LiveCapture(interface='lo', display_filter='http', only_summaries=False)
 message=[]
 count= 0
+check =[]
+start=False
 for packet in capture.sniff_continuously():
     #GET /c2?4157 HTTP/1.1\r\n
     packet=str(packet)
     try:
         data=packet.split("GET ")[1].split(" ")[0].split("/c")[1].split("?")[0]
-        count += 1 
-        message.append(data)
-        mess=''.join(message)
-        if(count==2):
-            os.system("clear")
-            newmess = binascii.unhexlify(mess).decode()
-            print(newmess)
-            count = 0
+        check.append(data) 
+        if(len(check)==6 and check=='2D2D2D'): 
+            start=True 
+            check =[]
+        elif(len(check)==6 and check=='2D2E2D'):
+            break
+        if(start):
+            count += 1 
+            message.append(data)
+            mess=''.join(message)
+            if(count==2):
+                os.system("clear")
+                newmess = binascii.unhexlify(mess).decode()
+                print(newmess)
+                count = 0
     except:
         continue
 capture.close()
